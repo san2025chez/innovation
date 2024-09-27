0ed181d67773f7383cd22a49086d2b47db9644e0
ana <vane2013sanchez@gmail.com>
2024-09-27 11:15:54 -0300

    update-styles cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,28 +9,21 @@ const ProjectCard = ({ title, description, image, onOpen }) => {
 
   return (
     <motion.div whileHover={{ scale: 1.05 }}>
-      <Card
-        className={classes.card}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onClick={onOpen}
-      >
+      <Card className={classes.card}>
         <CardMedia
           component="img"
           image={image}
           className={classes.caratula}
           title={title}
         />
-        {hover && (
-          <div className={classes.overlay}>
-            <Typography variant="h6" className={classes.title}>
-              {title}
-            </Typography>
-            <Typography className={classes.description}>
-              {description}
-            </Typography>
-          </div>
-        )}
+        <div className={classes.overlay}>
+          <Typography variant="h6" className={classes.title}>
+            {title}
+          </Typography>
+          <Typography className={classes.description}>
+            {description}
+          </Typography>
+        </div>
         <div className={classes.infoIconContainer}>
           <IconButton className={classes.infoIcon} onClick={onOpen}>
             <InfoIcon />
@@ -46,28 +39,30 @@ const useStyles = makeStyles((theme) => ({
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
+    minHeight: '200px',
     alignItems: 'center',
     overflow: 'hidden',
     transition: 'all 0.3s ease-in-out',
     cursor: 'pointer',
-    backgroundColor: '#000000', // Fondo negro
-    color: '#E0E0E0', // Color de texto por defecto
-    '&:hover': {
-      boxShadow: '0 6px 12px rgba(0, 0, 0, 0.3)',
-    },
+    backgroundColor: '#121212', // Darker background for a modern feel
+    color: '#FFFFFF', // White text for better contrast
+    height: 300, // Set a fixed height
+    borderRadius: 10,
+    justifyContent: 'space-between'
+
   },
   caratula: {
     width: '100%',
-    height: 'auto',
+    height: '200px', // Adjust height as needed
+    objectFit: 'cover', // Ensure image covers the entire area
   },
   overlay: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     width: '100%',
-    height: '30%',
-    background: 'rgba(0, 0, 0, 0.7)', // Fondo oscuro para el overlay
-    color: '#FFFFFF', // Texto en blanco
+    height: '100%', // Full height for a more immersive overlay
+    background: 'rgba(0, 0, 0, 0.7)',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -75,16 +70,19 @@ const useStyles = makeStyles((theme) => ({
     transition: 'opacity 0.3s ease-in-out',
     padding: theme.spacing(2),
     boxSizing: 'border-box',
+    flexGrow: 1,
   },
   title: {
     fontWeight: 'bold',
     fontSize: '1.2rem',
-    color: '#FFFFFF', // Color del título en blanco
+    color: '#FFFFFF',
   },
   description: {
     fontSize: '0.9rem',
     marginTop: theme.spacing(1),
-    color: '#E0E0E0', // Color de la descripción en gris claro
+    color: '#E0E0E0',
+    textAlign: 'justify',
+    hyphens: 'auto'
   },
   infoIconContainer: {
     position: 'absolute',
@@ -92,7 +90,7 @@ const useStyles = makeStyles((theme) => ({
     right: theme.spacing(1),
   },
   infoIcon: {
-    color: '#007ACC', // Color del icono en azul eléctrico
+    color: '#007ACC',
   },
 }));
 
